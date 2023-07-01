2ba9e0869b8c21009b11b79291cb2d9537ec8212
stephanus yogi <stephanusyogi12@gmail.com>
2023-07-01 20:13:44 +0700

    Progress

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -16,6 +16,7 @@ export const actions = {
     const email = formData.get("email")
     const password = formData.get("password")
 
+    let user
     try {
       const response = await axios.post(BACKEND_API+'/login', 
       'email='+email+'&password='+password, {
@@ -23,49 +24,37 @@ export const actions = {
           'Content-Type': 'application/x-www-form-urlencoded'
         }
       })
-      console.log(response)
+      user = response.data
     } catch (error) {
-      console.log(error)
-      // return fail(400, {
-      //   error: true,
-      //   message: error.response.data.message,
-      // }); 
+      return fail(400, {
+        error: true,
+        message: error.response.data.message,
+      }); 
     }
-    // const login = await axios.post(BACKEND_API+'/login', 
-    // 'email='+email+'&password='+password, {
-    //   headers: {
-    //     'Content-Type': 'application/x-www-form-urlencoded'
-    //   }
-    // })
-    // .then(function (response) {
-    //   console.log(response)
-    //   return {success:true, data:response}
-    // })
-    // .catch(function (error) {
-    //   console.log(error)
-    //   return {error:true, data:error}
-    // });
 
-    // if(login.status === 200){
-    //   cookies.set("access", "true", {
-    //     // send cookie for every page
-    //     path: '/',
-    //     // server side only cookie so you can't use `document.cookie`
-    //     httpOnly: true,
-    //     // only requests from same site can send cookies
-    //     // https://developer.mozilla.org/en-US/docs/Glossary/CSRF
-    //     sameSite: 'strict',
-    //     // set cookie to expire after a month
-    //     maxAge: 60 * 60 * 24 * 30})
-    //   cookies.set("user_data_access", JSON.stringify(login.data), {
-    //     path: '/',
-    //     httpOnly: true,
-    //     sameSite: 'strict',
-    //     maxAge: 60 * 60 * 24 * 30})
-    //   throw redirect(302, "/")
-    // }
-    return {
-      email
+    if(user){
+      cookies.set("access", "true", {
+        // send cookie for every page
+        path: '/',
+        // server side only cookie so you can't use `document.cookie`
+        httpOnly: true,
+        // only requests from same site can send cookies
+        // https://developer.mozilla.org/en-US/docs/Glossary/CSRF
+        sameSite: 'strict',
+        // set cookie to expire after a month
+        maxAge: 60 * 60 * 24 * 30})
+      cookies.set("user_data_access", JSON.stringify(user), {
+        path: '/',
+        httpOnly: true,
+        sameSite: 'strict',
+        maxAge: 60 * 60 * 24 * 30})
+      throw redirect(303, "/")
+    }else{
+      return {
+        error:true,
+        email,
+        message: 'Silahkan Coba Kembali'
+      }
     }
   }
 };
