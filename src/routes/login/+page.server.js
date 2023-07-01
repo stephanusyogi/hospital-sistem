dc1f04befa053358f3775cc7956157092e412cc1
stephanus yogi <stephanusyogi12@gmail.com>
2023-07-01 18:29:36 +0700

    progress

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -23,32 +23,36 @@ export const actions = {
       }
     })
     .then(function (response) {
-      cookies.set("access", "true", {
-        // send cookie for every page
-        path: '/',
-        // server side only cookie so you can't use `document.cookie`
-        httpOnly: true,
-        // only requests from same site can send cookies
-        // https://developer.mozilla.org/en-US/docs/Glossary/CSRF
-        sameSite: 'strict',
-        // set cookie to expire after a month
-        maxAge: 60 * 60 * 24 * 30})
-      cookies.set("user_data_access", JSON.stringify(response.data), {
-        path: '/',
-        httpOnly: true,
-        sameSite: 'strict',
-        maxAge: 60 * 60 * 24 * 30})
-      throw redirect(302, "/")
+      // cookies.set("access", "true", {
+      //   // send cookie for every page
+      //   path: '/',
+      //   // server side only cookie so you can't use `document.cookie`
+      //   httpOnly: true,
+      //   // only requests from same site can send cookies
+      //   // https://developer.mozilla.org/en-US/docs/Glossary/CSRF
+      //   sameSite: 'strict',
+      //   // set cookie to expire after a month
+      //   maxAge: 60 * 60 * 24 * 30})
+      // cookies.set("user_data_access", JSON.stringify(response.data), {
+      //   path: '/',
+      //   httpOnly: true,
+      //   sameSite: 'strict',
+      //   maxAge: 60 * 60 * 24 * 30})
+      // throw redirect(302, "/")
     })
     .catch(function (error) {
       console.log(error.response)
-      return {
-        error:true,
-        email: email,
-        message:"Invalid Username/Password"
-      };
+      // return {
+      //   error:true,
+      //   email: email,
+      //   message:"Invalid Username/Password"
+      // };
     });
     
-    return login;
+    return {
+      error:true,
+      email: email,
+      message:"Invalid Username/Password"
+    };;
   }
 };
